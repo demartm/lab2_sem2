892600933186218dbdb50707d7e1fec6e9db8330
demartm <artemis20242511@gmail.com>
2026-03-24 00:53:07 -0400

    code cleaning

--- main.c
+++ main.c
@@ -6,11 +6,15 @@
 #define Column 5
 
 void free_mx(int **mx, size_t row){
+
 if(mx){
-for(int i = 0; i < row; i++){
-free(mx[i]);
-mx[i] = NULL;
-}
+
+  for(int i = 0; i < row; i++){
+
+    free(mx[i]);
+    mx[i] = NULL;
+  }
+
 free(mx);
 mx = NULL;
 }
@@ -20,36 +24,31 @@ mx = NULL;
 
 bool print_mx(int **mx,size_t row,size_t col){
 
-if(mx){
+  if(mx){
 
-for(int i = 0; i < row; i++){
+    for(int i = 0; i < row; i++){
 
-  if(mx[i]){
+      if(mx[i]){
 
-      for(int j = 0 ; j < col;j++){
+          for(int j = 0 ; j < col;j++){
+            printf("%d ",mx[i][j]);
+          }
 
-        //if(mx[i][j]){
+      } else {
+      printf("[NULL]");
+      }
 
-        printf("%d ",mx[i][j]);
-
-      //   } else {
-
-      // printf("0 ");
-      //   }
+    printf("\n");
     }
-  }else{
-    printf("[NULL]");
+  return 1;
   }
-
-printf("\n");
-}
-return 1;
-}
 return 0;
 
 }
 int** init_mx(size_t row, size_t col){
+
 if(row && col){
+
   int **mx = (int**)malloc(row*sizeof(int*));
 
   if(mx){
@@ -86,7 +85,7 @@ if(mx){
       if(mx[i]){
 
         mx[i][j] = rand() % (rangeB + 1) + rangeA;
-      }
+      }//clean
     }
   }
 return 1;
@@ -96,19 +95,24 @@ return 0;
 }
 
 int **combine_mx(int **mx1, int **mx2, size_t row1, size_t col1, size_t row2, size_t col2){
+
 size_t new_mx_row = 0;
-if(row1>row2){
-new_mx_row = row1;
-}else{
-new_mx_row = row2;
+if(row1 > row2){
+  new_mx_row = row1;
+} else {
+  new_mx_row = row2;
 }
+
 size_t new_mx_col = col1+col2;
 
 int **new_mx = init_mx(new_mx_row,new_mx_col);
 
 for(int i = 0; i < row1; i++){
+
   for(int j = 0; j < col1; j++){
+
     if(mx1[i]){
+
       new_mx[i][j] = mx1[i][j];
     } else {
       new_mx[i][j] = 0;
@@ -117,46 +121,18 @@ for(int i = 0; i < row1; i++){
 }
 
 for(int i = 0; i < row2; i++){
+
   for(int j = 0; j < col2; j++){
-    //new_mx[i][col1+j] = mx2[i][j];
+
     if(mx2[i]){
+
       new_mx[i][col1+j] = mx2[i][j];
     } else {
+
       new_mx[i][col1+j] = 0;
     }
   }
 }
-
-
-// for(int i = 0; i < new_mx_row; i++){
-// for(int j = 0; j < col1; j++){
-// new_mx[i][j] = mx1[i][j];
-// }
-
-// for(int j = 0; j < col2; j++){
-// new_mx[i][col1+j] = mx2[i][j];
-// }
-
-// }
-
-
-
-// int **new_mx = init_mx(row,col);
-
-// for(int i = 0; i < row; i++){
-// if(!(mx1[i] && mx2[i])){
-// if(mx1[i]==NULL){
-// new_mx[i] = mx2[i];
-// }else{
-
-// new_mx[i] = mx1[i];
-//                           //(error if both null? if both different?)
-// }
-// }else{
-// new_mx[i] = mx1[i];
-// }
-// }
-
 
  return new_mx;
 }
@@ -165,34 +141,27 @@ for(int i = 0; i < row2; i++){
 int main()
 {
 srand(time(0));
-    //printf("Hello World!\n");
-//int mx[10][10]={0};
-//printf("%d",sizeof(mx)/sizeof(mx[0]));
-int **mx1 = init_mx(Row,Column);
+
+int **mx1 = init_mx(Row,Column+5);
 int **mx2 = init_mx(Row+5,Column);
 
-//print_mx(mx,10,10);
- fill_mx(mx1,Row,Column,0,100);
-  //free(mx1[5]);
-  //mx1[5] = NULL;
 
- fill_mx(mx2,Row+5,Column,0,100);
- // free(mx2[8]);
- // mx2[8] = NULL;
-int **mx3 = combine_mx(mx1,mx2,Row,Column,Row+5, Column);
-// print_mx(mx,10,10);
+fill_mx(mx1,Row,Column+5,0,100);
 
-// printf("\n-----------------------------\n\n");
-print_mx(mx1,Row,Column);
+fill_mx(mx2,Row+5,Column,0,100);
+int **mx3 = combine_mx(mx1,mx2,Row,Column+5,Row+5, Column);
+
+print_mx(mx1,Row,Column+5);
 printf("\n-----------------------------\n\n");
-print_mx(mx2,Row,Column);
+
+print_mx(mx2,Row+5,Column);
 printf("\n-----------------------------\n\n");
-print_mx(mx3,Row+5,Column+Column);
+
+print_mx(mx3,Row+5,Column+Column+5);
 free_mx(mx1,Row);
 free_mx(mx2,Row+5);
 
 free_mx(mx3,Row+5);
-// int a = 20 * (int)NULL;
-// printf("%d",a);
+
     return 0;
 }
