e38e58c3fb0366c749c494051fdeb9fef0ad4a95
demartm <artemis20242511@gmail.com>
2026-03-24 00:40:57 -0400

    code cleaning and logic improving

--- main.c
+++ main.c
@@ -3,76 +3,67 @@
 #include <time.h>
 #include <stdlib.h>
 #define Row 15
-#define Column 1
+#define Column 5
 
 void free_mx(int **mx, size_t row){
-if(mx){
-for(int i = 0; i < row; i++){
-free(mx[i]);
-mx[i] = NULL;
-}
-free(mx);
-mx = NULL;
-}
+  if(mx){
+    for(int i = 0; i < row; i++){
+      free(mx[i]);
+      mx[i] = NULL;
+    }
+    free(mx);
+    mx = NULL;
+  }
 
 }
 
 
 bool print_mx(int **mx,size_t row,size_t col){
 
-if(mx){
+  if(mx){
 
-for(int i = 0; i < row; i++){
-
-  if(mx[i]){
-
-      for(int j = 0 ; j < col;j++){
-
-        if(mx[i][j]){
-
-        printf("%d ",mx[i][j]);
-
+    for(int i = 0; i < row; i++){
+        if(mx[i]){
+            for(int j = 0 ; j < col;j++){
+              printf("%d ",mx[i][j]);
+            }
         } else {
-
-      printf("0 ");
-        }
+        printf("[NULL]");
+      }
+    printf("\n");
     }
-  }else{
-    printf("[NULL]");
+  return 1;
   }
-
-printf("\n");
-}
-return 1;
-}
 return 0;
 
 }
 int** init_mx(size_t row, size_t col){
-if(row && col){
-  int **mx = (int**)malloc(row*sizeof(int*));
 
-  if(mx){
+  if(row && col){
 
-    for(int i = 0; i < row; i++){
+    int **mx = (int**)malloc(row*sizeof(int*));
 
-      mx[i] = (int*)malloc(col*sizeof(int));
+      if(mx){
 
-      if(!mx[i]){
+        for(int i = 0; i < row; i++){
 
-        free_mx(mx,i);
-        i = row;
+          mx[i] = (int*)malloc(col*sizeof(int));
+
+          if(!mx[i]){
+
+            free_mx(mx,i);
+            i = row;
+          }
+
+        }
+
+      } else {
+      free(mx);
+      mx = NULL;
       }
 
-    }
-
-  } else {
-    free(mx);
-    mx = NULL;
-    }
-
-return mx;
-}
+  return mx;
+  }
 return 0;
 }
 
@@ -122,19 +113,19 @@ return 0;
 
 int main()
 {
+
 srand(time(0));
-    //printf("Hello World!\n");
-//int mx[10][10]={0};
-//printf("%d",sizeof(mx)/sizeof(mx[0]));
+
 int **mx = init_mx(Row,Column);
+
 if(!mx){
-printf("error initializing matrix");
-return 0;
+  printf("error initializing matrix");
+  return 0;
 }
-//print_mx(mx,10,10);
+
 fill_mx(mx,Row,Column,0,100);
- free(mx[0]);
- mx[0] = NULL;
+free(mx[0]);
+mx[0] = NULL;
 
 print_mx(mx,Row,Column);
 
@@ -145,5 +136,7 @@ int **test = transponse_matrix(mx,Row,Column);
 print_mx(test,Column,Row);
 free_mx(mx,Row);
 free_mx(test,Column);
+
 return 0;
+
 }
